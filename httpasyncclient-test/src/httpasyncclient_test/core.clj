6b7e6b7b7462472b400fe95708eff2b8758c444e
Phil Calçado <pcalcado@gmail.com>
2011-06-04 16:28:42 +0100

    ran the two first benchmarks

--- httpasyncclient-test/src/httpasyncclient_test/core.clj
+++ httpasyncclient-test/src/httpasyncclient_test/core.clj
@@ -3,13 +3,37 @@
   (:use net.cgrand.moustache)
   (:use [ring.adapter.jetty :only [run-jetty]])  
   (:use [ring.middleware file params session])
+  (:require [clojure.contrib.duck-streams :as duck-streams])
+  (:require [clojure.contrib.jmx :as jmx])
   (import [java.io BufferedReader InputStreamReader PrintWriter ]
           [java.util.concurrent Executors]
           [org.apache.http.client.methods HttpGet]
           [org.apache.http.impl.conn.tsccm ThreadSafeClientConnManager]
           [org.apache.http.impl.client DefaultHttpClient BasicResponseHandler]))
 
-;server
+;;default client
+(def max-connections 50)
+(def http-client (DefaultHttpClient.
+                   (doto (new ThreadSafeClientConnManager)
+                     (.setMaxTotal max-connections)
+                     (.setDefaultMaxPerRoute max-connections))))
+
+(defn GET [uri] (. http-client execute (new HttpGet uri) (new BasicResponseHandler)))
+
+
+;;util
+(defn run-out-of-ports []
+  (let [more-ports-than-allowed (+ 1
+                                   (long (jmx/read "java.lang:type=OperatingSystem" :MaxFileDescriptorCount)))]
+    (doall
+     (. (Executors/newFixedThreadPool more-ports-than-allowed) invokeAll
+        (map (fn [_] #(GET "http://localhost:8765/quick/1")) (range more-ports-than-allowed))))))
+
+(def log-filename "ports.log")
+
+(defn log [txt] (duck-streams/append-spit log-filename (str  txt "\n")))
+
+;;server
 (def some-long-time (long 2000))
 (def port 8765)
 
@@ -23,7 +47,7 @@
   (Thread/sleep (/ some-long-time 10))
   (System/currentTimeMillis))
 
-;TODO: apparently {:get [request-number "@"]} causes a "string is not fn" exception. bug in moustache?
+;;TODO: apparently {:get [request-number "@"]} causes a "string is not fn" exception. bug in moustache?
 (defn app-for [response-fn] (app [request-number]
                                  {:get ["" request-number "@" (response-fn)]}))
 
@@ -34,16 +58,8 @@
 (defn start-server! []
   (. (Thread. #(run-jetty #'webservice-app {:port port})) start))
 
-;default client
+;;requests
 (def number-of-threads 170)
-(def http-client (DefaultHttpClient.
-                   (doto (new ThreadSafeClientConnManager)
-                     (.setMaxTotal number-of-threads)
-                     (.setDefaultMaxPerRoute number-of-threads))))
-
-(defn GET [uri] (. http-client execute (new HttpGet uri) (new BasicResponseHandler)))
-
-;requests
 (defn request! [endpoint request-number]
   (GET (str "http://localhost:" port "/" endpoint "/" request-number )))
 
@@ -55,36 +71,49 @@
 
 (def dogpile (map make-request (range number-of-threads)))
 
-;util
 
-(defmacro timed [& code]
-  `(let [start# (System/currentTimeMillis)]
-     ~@code
-     (let [finish# (System/currentTimeMillis)]
-       (println "Took: " finish# "-" start# "=" (- finish# start#)))))
-
-(defmacro benchmark [& code]
-  `(doall (timed ~@code)))
-
-;strategies
-;serial
+;;strategies
+;;serial
 (defn serialised-dogpile! []
-  (map #(apply % nil) dogpile) :over)
-;Took:  1307120445540 - 1307119759931 = 685609
+  (map #(apply % nil) dogpile))
 
-;threaded+futures
+;;naive-threads
+
+
+;;threaded+futures
 (defn threaded-dogpile! []
   (let [executor-service (Executors/newFixedThreadPool number-of-threads)]
     (. executor-service invokeAll dogpile)))
-;per route = number-of-threads   => Took:  1307121685128 - 1307121679647 = 5481
-;per route = number-of-threads/2 => Took:  1307122131996 - 1307122121225 = 10771
-;per route = 2                   => Took:  1307122084873 - 1307121742519 = 342354
 
-;enhanced futures
-(doall (. (Executors/newFixedThreadPool 2000) invokeAll (map (fn [_] #(GET "http://localhost:8765/quick/1")) (range 20000))))
-
-;apache http async?
+;;enhanced futures
 
 
-;ning?
+
+;;apache http async?
+
+
+;;ning?
+
+
+;;benchmarks
+(defmacro benchmark [& code]
+  `(let [start# (System/currentTimeMillis)]
+     (log (str "STARTED: " '~@code))
+     (doall ~@code)
+     (let [finish# (System/currentTimeMillis)]
+       (log (str "FINISHED:" '~@code  "Took: " finish# "-" start# "=" (- finish# start#))))))
+
+(defn dump-file-count! []
+  (. (Thread.
+      (fn []
+        (loop []
+          (log (str (System/currentTimeMillis) "," (jmx/read "java.lang:type=OperatingSystem" :OpenFileDescriptorCount)))
+          (Thread/sleep 500)
+          (recur))))     
+     start))
+
+(defn init-system! []
+  (log "==>Starting")
+  (start-server!)
+  (dump-file-count!))
 
